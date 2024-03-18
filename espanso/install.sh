a430afdafac91ae0aaf312c2c0a7770bffdf6279
snoguchi1212 <snoguchi.nbsr@gmail.com>
2024-03-18 13:47:51 +0900

    ✨ update espanso settings

--- espanso/install.sh
+++ espanso/install.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
 brew install --cask espanso
+
+cat <<-EOF >"${DOTFILES}/espanso/match/password.local.yml"
+matches:
+  - triggers: [":password:"]
+    replace: "{{output}}"
+    vars:
+      - name: output
+        type: shell
+        params:
+          cmd: "security find-generic-password -a 'password' -w"
+EOF
+
+echo "password.local.yml created in espanso/match/ directory!"
