2785ef17330bdb29fa0dc359ed5a36b1d621684f
snoguchi1212 <noguchi_shoki@beenos.com>
2025-02-23 23:41:28 +0900

    ✨ bin と .local/bin の scripts を参照するように修正

--- zsh/scripts/path.zsh
+++ zsh/scripts/path.zsh
@@ -10,7 +10,11 @@ source_if_exists() {
 # local file
 # ------------------
 # bin
-export PATH="$DOTFILES/bin/scripts:$DOTFILES/bin/.local/scripts:$PATH"
+export PATH="$DOTFILES/bin:$DOTFILES/bin/scripts:$PATH"
+
+for dir in $(find "$HOME/.local/scripts" -type d -maxdepth 1); do
+	export PATH="$dir:$PATH"
+done
 
 # asdf
 export PATH="/opt/homebrew/opt/asdf/libexec/asdf.sh:$PATH"
